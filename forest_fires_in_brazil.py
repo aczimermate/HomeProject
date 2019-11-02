6509d95c4b5ac781b5486f421ad6388da2ce33c4
aczimermate <czimer21@gmail.com>
2019-11-02 09:14:06 +0100

    Adding visualizations part1

--- forest_fires_in_brazil.py
+++ forest_fires_in_brazil.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
 
-file = "c:/Users/Czimer Máté/Documents/PythonScripts/csvs/amazon.csv"
+file = "c:/git/HomeProject/csvs/amazon.csv"
 
 # Create DataFrame object
 df = pd.read_csv(file, encoding="latin1")
 # print(df.head(),'\n', df.tail())
 
-no_per_year = df.groupby()["year"].sum("number")
-print(no_per_year)
+# Create some plots to visualize the dataset
+df.plot(kind="scatter", x="year", y="number")
+
